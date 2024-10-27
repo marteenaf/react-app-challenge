151883ecb3051e984c4da05ca6123d9379eba416
marteena <martina.fatato@gmail.com>
2024-10-27 17:49:16 +0000

    (doc): Updated docs

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,12 @@ import './styles/globals.css'
 import LoginPage from './pages/LoginPage'
 import DashboardPage from './pages/DashboardPage'
 import { Routes, Route } from 'react-router-dom';
+
+/**
+ * Main app component
+ * Using some very simple Routes to connect the login and dashboard pages
+ * @returns {JSX.Element}
+ */
 function App() {
 
   return (
